7db4b10855af1767f1d4fc476d246e0bf1a5e700
HugoAlonso2025 <hugoalonsofernandez03@gmail.com>
2026-04-13 12:55:40 +0200

    Logica de los npc
    Los NPC en forma de prefab son instanciados por una corutina en un tiempo x, buscan sus posiciones de pedidos y se dirijen a la primera libre. Una vez reciben el pedido se van a la salida y desaparecen

---
+++ Assets/Bar/Scripts/CustomerBehaviour.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using UnityEngine;
+
+public class CustomerBehaviour : MonoBehaviour
+{
+    [SerializeField] RequestController[] requests;
+    GameObject deliverAssigned;
+    Vector3 target;
+    [SerializeField] float speed;
+    [SerializeField] Vector3 radius;
+    [SerializeField] float mRadius;
+    GameObject exitPos;
+    bool posReached = false;
+    [SerializeField] LayerMask askPos;
+    [SerializeField] LayerMask exitMask;
+    RequestController request;
+    bool onExit = false;
+
+    void AssignDeliver()
+    {
+        for (int i = 0; i < requests.Length; i++)
+        {
+            if (!requests[i].isTaken)
+            {
+                request = requests[i];
+                deliverAssigned = request.gameObject;
+                request = deliverAssigned.GetComponent<RequestController>();
+                continue;
+            }
+        }
+    }
+
+    private void Start()
+    {
+        requests = FindObjectsOfType<RequestController>();
+        exitPos = GameObject.Find("SpawnPos");
+        AssignDeliver();
+    }
+
+    private void Update()
+    {
+        if (deliverAssigned != null && !posReached)
+        {
+            CheckPositionReached();
+            MoveTowardsDeliver();
+        }
+
+        if(request.glassPlaced)
+        {
+            request.glassPlaced = false;
+            DoAnimation();
+        }
+
+        if(onExit)
+        {
+            MoveTowardsExit();
+        }
+
+        if(Physics.CheckSphere(transform.position, mRadius, exitMask) && onExit)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    void CheckPositionReached()
+    {
+        if(Physics.CheckBox(transform.position, radius, Quaternion.identity,  askPos))
+        {
+            posReached = true;
+            request.isTaken = true;
+            request.AskForDrink();
+        }
+    }
+
+    void MoveTowardsDeliver()
+    {
+        target = new Vector3(deliverAssigned.transform.position.x, transform.position.y, deliverAssigned.transform.position.z);
+        transform.LookAt(target);
+        transform.position += transform.forward * speed *  Time.deltaTime;
+    }
+
+    void DoAnimation()
+    {
+        StartCoroutine(AnimationTime());
+    }
+
+    void MoveTowardsExit()
+    {
+        target = new Vector3(exitPos.transform.position.x, transform.position.y, exitPos.transform.position.z);
+        transform.LookAt(exitPos.transform);
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireCube(transform.position, radius);
+        Gizmos.DrawWireSphere(transform.position, mRadius);
+    }
+
+    IEnumerator AnimationTime()
+    {
+        yield return new WaitForSeconds(2);
+        onExit = true;
+    }
+}
